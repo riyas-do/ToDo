f7fad2c2d54d033e593d410e2ccea0d13cc7d0d7
RiyasNG <gafoorriyas54@gmail.com>
2026-03-18 00:40:41 +0530

    tasks_db_fixes

--- Controllers/TaskController/readTask.ts
+++ Controllers/TaskController/readTask.ts
@@ -4,9 +4,19 @@ import type { Request, Response } from "express";
 export default async function getAllTask(req: Request, res: Response){
     try{
       const userId = req.headers.user;
-      const tasksData = await client.query(`
-           SELECT * FROM tasks where user_id = $1`, [userId] );
+      const filter = req.query.filter;
+      let query = `SELECT * FROM tasks where user_id = $1 `;
+      const today = new Date().toISOString().split('T')[0];
+      if( filter === 'today'){ 
+         query += `and Date(due_date) = '${today}'`;
+        
+      }else if( filter === 'upcoming'){
+         query += `and Date(due_date) > '${today}'`
+      }
+      query += ` ORDER BY Date(due_date)`;
+      const tasksData = await client.query(`${query}`, [userId] );
       const tasks = tasksData.rows;
+
       if(tasks?.length){
         return res.status(200).send({
             success: true,
@@ -35,13 +45,23 @@ export async function searchTasks(req: Request, res: Response){
   try{
    const userId = req.headers.user;
    const searchParam = req.query.search;
+   const filter = req.query.filter;
     if(!searchParam){
         return res.status(404).send({
             success: false,
             message: 'No data found!'
         })
     }
-    const searchResult = await client.query(`SELECT * FROM tasks WHERE user_id = $1 and description like '%' || $2 || '%'`, [userId, searchParam]);
+    let query = `SELECT * FROM tasks WHERE user_id = $1 and description like '%' || $2 || '%'`
+    const today = new Date().toISOString().split('T')[0];
+      if( filter === 'today'){ 
+         query += `and Date(due_date) = '${today}'`;
+        
+      }else if( filter === 'upcoming'){
+         query += `and Date(due_date) > '${today}'`
+      }
+    query += ` ORDER BY Date(due_date)`;
+    const searchResult = await client.query(query, [userId, searchParam]);
     const searchData = searchResult.rows; 
     if(!searchData.length){
        return res.status(404).send({
